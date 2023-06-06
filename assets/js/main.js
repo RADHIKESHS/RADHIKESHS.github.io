881dc211b1818307e1ad15c0b14b561e2ddd9a4e
Radhikesh <radhikesh310@gmail.com>
2023-06-06 14:40:54 +0530

    portfolio final submission4

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,5 +98,5 @@ sr.reveal('.home__data, .home__social , .contact__container, .footer__container'
 sr.reveal('.home__image', {origin: 'bottom'})
 sr.reveal('.about__data, .skills__data',{origin: 'left'})
 sr.reveal('.about__image, .skills__content', {origin: 'right'})
-sr.reveal('.services__card, .projects__card', {interval: 100})
+sr.reveal('.services__card, .projects__card', {interval: 70})
 
