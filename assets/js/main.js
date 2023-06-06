2c8e64b9586af08f57d6673923f4e985be0be0c8
Radhikesh <radhikesh310@gmail.com>
2023-06-06 12:26:29 +0530

    portfolio final submission

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,3 +85,18 @@ let scrollActive = () =>{
 window.addEventListener('scroll',scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+let sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration:2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.home__data, .home__social , .contact__container, .footer__container')
+sr.reveal('.home__image', {origin: 'bottom'})
+sr.reveal('.about__data, .skills__data',{origin: 'left'})
+sr.reveal('.about__image, .skills__content', {origin: 'right'})
+sr.reveal('.services__card, .projects__card', {interval: 100})
+
